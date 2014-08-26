887f8f3239a2c8fc7b4d34cacc2d968c6394a605
Wolfger Schramm <wolfger@spearwolf.de>
2014-08-26 11:40:42 +0200

    moved log to papa.Logger

--- src/papa/log.js
+++ src/papa/log.js
@@ -3,54 +3,53 @@
 
     module.exports = function(papa) {
 
-        papa.Module('log', function(api) {
+        var api = papa.Logger = {};
 
-            api.options = {
-                DEBUG: true,
-                INFO: true,
-                WARN: true,
-                ERROR: true
+        api.options = {
+            DEBUG: true,
+            INFO: true,
+            WARN: true,
+            ERROR: true
+        };
+
+        var any_log = function() {
+            return api.options.DEBUG || api.options.INFO || api.options.WARN || api.options.ERROR;
+        };
+
+        var root = window || global || this;
+
+        if ('undefined' !== typeof root.console) {
+
+            api.debug = function() {
+                if (api.options.DEBUG) console.debug.apply(console, arguments);
+            };
+            api.info = function() {
+                if (api.options.INFO) console.info.apply(console, arguments);
+            };
+            api.warn = function() {
+                if (api.options.WARN) console.warn.apply(console, arguments);
+            };
+            api.error = function() {
+                if (api.options.ERROR) console.error.apply(console, arguments);
             };
 
-            var any_log = function() {
-                return api.options.DEBUG || api.options.INFO || api.options.WARN || api.options.ERROR;
-            };
-
-            var root = window || global || this;
-
-            if ('undefined' !== typeof root.console) {
-
-                api.debug = function() {
-                    if (api.options.DEBUG) console.debug.apply(console, arguments);
+            if ('function' === typeof root.console.group) {
+                api.group = function(name, fn) {
+                    var any = any_log();
+                    if (any) console.group(name);
+                    fn();
+                    if (any) console.groupEnd();
                 };
-                api.info = function() {
-                    if (api.options.INFO) console.info.apply(console, arguments);
-                };
-                api.warn = function() {
-                    if (api.options.WARN) console.warn.apply(console, arguments);
-                };
-                api.error = function() {
-                    if (api.options.ERROR) console.error.apply(console, arguments);
-                };
-
-                if ('function' === typeof root.console.group) {
-                    api.group = function(name, fn) {
-                        var any = any_log();
-                        if (any) console.group(name);
-                        fn();
-                        if (any) console.groupEnd();
-                    };
-                } else {
-                    api.group = function(_, fn) {
-                        fn();
-                    };
-                }
-
             } else {
-
-                api.debug = api.info = api.warn = api.error = function(){};
+                api.group = function(_, fn) {
+                    fn();
+                };
             }
-        });
+
+        } else {
+
+            api.debug = api.info = api.warn = api.error = function(){};
+        }
     };
 
 })();
