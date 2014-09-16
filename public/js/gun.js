3a04d678557691fce0162ffd1d34141767f6c065
Wolfger Schramm <wolfger@spearwolf.de>
2014-09-16 10:12:41 +0200

    fixed gun.Logger

--- public/js/gun.js
+++ public/js/gun.js
@@ -518,55 +518,77 @@
 
     module.exports = function(gun) {
 
-        var api = gun.Logger = {};
-
-        api.options = {
-            DEBUG: true,
-            INFO: true,
-            WARN: true,
-            ERROR: true
-        };
-
-        var any_log = function() {
-            return api.options.DEBUG || api.options.INFO || api.options.WARN || api.options.ERROR;
-        };
-
+        var api = gun.Logger = Object.create(null);
         var root = _dereq_('./root')();
 
-        if ('undefined' !== typeof root.console) {
+        api.options = {
+            //DEBUG: true,
+            //INFO: true,
+            //WARN: true,
+            //ERROR: true
+        };
 
-            api.debug = function() {
-                if (api.options.DEBUG) root.console.debug.apply(root.console, arguments);
-            };
-            api.info = function() {
-                if (api.options.INFO) root.console.info.apply(root.console, arguments);
-            };
-            api.warn = function() {
-                if (api.options.WARN) root.console.warn.apply(root.console, arguments);
-            };
-            api.error = function() {
-                if (api.options.ERROR) root.console.error.apply(root.console, arguments);
-            };
-
-            if ('function' === typeof root.console.group) {
-                api.group = function(name, fn) {
-                    var any = any_log();
-                    if (any) root.console.group(name);
-                    fn();
-                    if (any) root.console.groupEnd();
-                };
-            } else {
-                api.group = function(_, fn) {
-                    fn();
-                };
+        Object.defineProperty(api.options, 'DEBUG', {
+            get: function(){ return this._DEBUG; },
+            set: function(enable){
+                this._DEBUG = enable;
+                var con;
+                try { con = console; } catch (err) {}
+                if (enable && con && 'undefined' !== typeof con.debug) {
+                    api.debug = con.debug.bind(con);
+                } else {
+                    api.debug = function(){};
+                }
             }
+        });
 
-        } else {
+        Object.defineProperty(api.options, 'INFO', {
+            get: function(){ return this._INFO; },
+            set: function(enable){
+                this._INFO = enable;
+                var con;
+                try { con = console; } catch (err) {}
+                if (enable && con && 'undefined' !== typeof con.info) {
+                    api.info = con.info.bind(con);
+                } else {
+                    api.info = function(){};
+                }
+            }
+        });
 
-            api.debug = api.info = api.warn = api.error = function(){};
-        }
+        Object.defineProperty(api.options, 'WARN', {
+            get: function(){ return this._WARN; },
+            set: function(enable){
+                this._WARN = enable;
+                var con;
+                try { con = console; } catch (err) {}
+                if (enable && con && 'undefined' !== typeof con.warn) {
+                    api.warn = con.warn.bind(con);
+                } else {
+                    api.warn = function(){};
+                }
+            }
+        });
+
+        Object.defineProperty(api.options, 'ERROR', {
+            get: function(){ return this._ERROR; },
+            set: function(enable){
+                this._ERROR = enable;
+                var con;
+                try { con = console; } catch (err) {}
+                if (enable && con && 'undefined' !== typeof con.error) {
+                    api.error = con.error.bind(con);
+                } else {
+                    api.error = function(){};
+                }
+            }
+        });
+
+        api.options.DEBUG = true;
+        api.options.INFO = true;
+        api.options.WARN = true;
+        api.options.ERROR = true;
     };
-
 })();
 // vim: et ts=4 sts=4 sw=4
 
